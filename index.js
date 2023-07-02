721a809129f0280f54158d5f663ce4a1a5fb8c12
secretariuss <secretarius@ukr.net>
2023-07-01 22:44:19 -0300

    added some function for generate readme file

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeFile(readme, data) {
+  fs.appendFile(`${readme}.md`, data, (err) => err ? console.error(err) : console.log(`${readme}.md created.`));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function userChoises() {
+  return inquirer
+    .prompt([
+
+      {
+        name: 'title',
+        message: 'What is the title of this application?',
+        type: 'input'
+      },
+      {
+        name: 'description',
+        message: 'What does this application do?',
+        type: 'input'
+      },
+      {
+        name: 'install',
+        message: 'How do I install this application?',
+        type: 'input'
+      },
+      {
+        name: 'usage',
+        message: 'How do I use this application?',
+        type: 'input'
+      },
+      {
+        name: 'license',
+        message: 'Select license for this application:',
+        type: 'list',
+        choices: ["Apache license 2.0"]
+      },
+      {
+        name: 'contributing',
+        message: 'How do I contribute to this application?',
+        type: 'input'
+      },
+      {
+        name: 'testing',
+        message: 'How can I test this application?',
+        type: 'input'
+      },
+      {
+        name: 'username',
+        message: 'What is your Github username?',
+        type: 'input'
+      },
+      {
+        name: 'email',
+        message: 'What is your e-mail?',
+        type: 'input'
+      },
+      {
+        name: 'readme',
+        message: 'What do you want to call this readme?',
+        type: 'input'
+      },
+    ])
+};
+
+async function init() {
+  let reply = await userChoises();
+  writeFile((reply.readme), (generateMarkdown(reply)));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,72 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseArr = ["Apache license 2.0"]
+
+function renderLicenseBadge(license) {
+  if (license === licenseArr[0]) {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  }
+  else {
+    return ""
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === licenseArr[0]) {
+    return `[${licenseArr[0]}](https://opensource.org/licenses/Apache-2.0)`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === licenseArr[0]) {
+    return `Read more about ${licenseArr[0]} here:`
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Badges](#badges)
+  * [How to Contribute](#how-to-contribute)
+  * [Tests](#tests)
+  * [Questions?](#questions)
+  
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+  ## How to Contribute
+  [Contributor Covenant](https://www.contributor-covenant.org/)  
+  ${data.contributing}
+  ## Tests
+  ${data.testing}
+  ## Questions?
+  ### Reach me here: 
+  [${data.username}](https://github.com/${data.username})  
+  ${data.email}`;
+
 }
 
 module.exports = generateMarkdown;
